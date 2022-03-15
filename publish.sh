c71907c0b4bd8f84dbff6dd0466e3be2c21701b0
Mark Eschbach <meschbach@gmail.com>
2022-03-14 21:56:12 -0700

    also build for linux/arm/v6

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t meschbach/vault-k8s-unsealer:$1 --push  .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t meschbach/vault-k8s-unsealer:$1 --push  .
